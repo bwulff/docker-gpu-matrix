85655602a146c46e47ac918f16fc8325d8981f3e
Ben Wulff <benjamin.wulff.de@ieee.org>
2018-09-01 14:48:14 +0200

    activated debug mode in server

--- gpudash/__init__.py
+++ gpudash/__init__.py
@@ -4,17 +4,19 @@ import docker
 from funcs import get_gpu_assigments
 
 num_gpus = int(os.getenv('NUM_GPUS', '1'))
+docker_url = os.getenv('DOCKER_URL', '/var/run/docker.sock')
 docker_client = docker.DockerClient(
-    base_url=os.getenv('DOCKER_URL', '/var/run/docker.sock'),
+    base_url=docker_url,
     user_agent='GPU Dashboard 0.1.0',
     version='auto')
+print("Using Docker deamon at {}".format(docker_url))
 
 # create application
 app = Flask(__name__)
 logger = app.logger
 
 @app.route(
-    '/gpu_assigments.html',
+    '/gpu_matrix.html',
     methods=['GET']
 )
 def serve_gpu_assigments():
--- server.py
+++ server.py
@@ -5,4 +5,4 @@ ip = os.getenv('BIND_IP', '0.0.0.0')
 port = int(os.getenv('BIND_PORT', 80))
 
 print("Starting server on {} port {}".format(ip, port))
-app.run(host=ip, port=port)
+app.run(host=ip, port=port, debug=True)
