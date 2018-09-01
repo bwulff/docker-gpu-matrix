9ecfe126b1676ed0c2db98d03aa54bf6dd79866f
Ben Wulff <benjamin.wulff.de@ieee.org>
2018-09-01 14:52:52 +0200

    update

--- gpudash/funcs.py
+++ gpudash/funcs.py
@@ -6,11 +6,11 @@ def get_gpu_assigments(docker_client, num_gpus):
         assignments[container.short_id] = [0] * num_gpus
         for var in container.attrs['Config']['Env']:
     		if var.startswith('NVIDIA_VISIBLE_DEVICES'):
-    			val = var.split('=', 1)[1]
-                if val == 'all':
+    			value = var.split('=', 1)[1]
+                if value.startwith('all'):
                     assignments[container.short_id] = [1] * num_gpus
                 else:
-                    for gpu in val.split(','):
+                    for gpu in value.split(','):
                         gpu_num = int(gpu.strip())
                         assignments[container.short_id][gpu_num] = 1
     return containers, assignments
